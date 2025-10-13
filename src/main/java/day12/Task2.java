73da047f1b8c552d4ec7d9d8b33f634ef487739a
DenisK <35003466+donz99@users.noreply.github.com>
2025-10-13 13:11:52 +0300

    completing task2 and task3 for day 12

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -1,7 +1,22 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task2 {
+
     public static void main(String[] args) {
+        List<Integer> list1=new ArrayList();
+        meth1(0,30,list1);
+        meth1(300,350,list1);
+        System.out.println(list1);
+    }
+
+    public static void meth1(int number1, int number2,List list){
+        for (int i = number1; i < number2+1; i++) {
+            if (i%2!=1&&i!=0)
+            list.add(i);
+        }
 
     }
 }
--- src/main/java/day12/task3/Task3.java
+++ src/main/java/day12/task3/Task3.java
@@ -1,7 +1,42 @@
 package day12.task3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Task3 {
     public static void main(String[] args) {
+        MusicBand mb1=new MusicBand("HolyDayBoy",1990);
+        MusicBand mb2=new MusicBand("Marsianin",1997);
+        MusicBand mb3=new MusicBand("SecondtoMars",1999);
+        MusicBand mb4=new MusicBand("Agutin",1995);
+        MusicBand mb5=new MusicBand("Mia",1992);
+        MusicBand mb6=new MusicBand("Gaga",2001);
+        MusicBand mb7=new MusicBand("RollsRoys",2005);
+        MusicBand mb8=new MusicBand("Mamamia",2007);
+        MusicBand mb9=new MusicBand("Roling",2002);
+        MusicBand mb10=new MusicBand("BadStreet",2001);
+        MusicBand mb11=new MusicBand("BritneySpears",2000);
+
+        List<MusicBand> listMB=new ArrayList<>(Arrays.asList(mb1,mb2,mb3,mb4,mb5,mb6,mb7,mb8,mb9,mb10,mb11));
+        System.out.println(listMB);
+        Collections.shuffle(listMB);
+        System.out.println(listMB);
+        List<MusicBand> listAfter2000=groupsAfter2000(listMB);
+        System.out.println(listAfter2000);
+
+    }
+
+    public static List<MusicBand> groupsAfter2000(List<MusicBand> bands){
+        List<MusicBand> after2000=new ArrayList<>();
+        for(MusicBand musicBand:bands){
+            if (musicBand.getYear()>2000)
+                after2000.add(musicBand);
+        }
+
+        return after2000;
+
 
     }
 }
