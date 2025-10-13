d448e6edabdecd13105ef76736ed66eeb3c007b3
DenisK <35003466+donz99@users.noreply.github.com>
2025-10-13 12:01:36 +0300

    completing task1 for day 12

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,20 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
+        List<String> list1=new ArrayList<>();
+        list1= new ArrayList<>(Arrays.asList("BMW","Mersedes","Audi","Wolk","Reno"));
+        System.out.println(list1);
+        list1.add("MAZ");
+        System.out.println(list1);
+        list1.add(2,"Geely");
+        System.out.println(list1);
+        list1.remove(0);
+        System.out.println(list1);
 
     }
 }
