1bc377a1038d1cb6c3570f0d5d3c9a5f9216b71a
DenisK <35003466+donz99@users.noreply.github.com>
2025-10-14 20:57:25 +0300

    completing task4 for day 12

---
+++ src/main/java/day12/task4/MusicBand.java
@@ -0,0 +1,80 @@
+package day12.task4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MusicBand {
+    private String groupName;
+    private int year;
+    private List<String> groupMember;
+
+    public static void transferMembers(MusicBand musicBand1, MusicBand musicBand2){
+        List<String> listGroupMemberAmount=new ArrayList<>();
+        System.out.println(listGroupMemberAmount);
+        listGroupMemberAmount.addAll(musicBand1.getGroupMember());
+        System.out.println(listGroupMemberAmount);
+        listGroupMemberAmount.addAll(musicBand2.getGroupMember());
+        System.out.println(listGroupMemberAmount);
+        musicBand2.setGroupMember(listGroupMemberAmount);
+        System.out.println(musicBand2);
+        musicBand1.setGroupMember(new ArrayList<String>(Collections.emptyList()));
+        System.out.println(musicBand1);
+    }
+
+    public List<String> getGroupMember() {
+        return groupMember;
+    }
+
+    public void setGroupMember(List<String> groupMember) {
+        this.groupMember = groupMember;
+    }
+
+
+
+    public void printMembers(){
+        System.out.println(groupMember);
+    }
+
+    public List<String> getMembers(){
+        return groupMember;
+    }
+
+    MusicBand(String name, int year, List<String> groupMember ){
+        this.groupName =name;
+        this.year=year;
+        this.groupMember=groupMember;
+    }
+
+//    public void addGroupMember(String... groupMember){
+//        for (String member : groupMember){
+//            this.groupMember=member;
+//        }
+//    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "MusicBand{" +
+                "groupName='" + groupName + '\'' +
+                ", year=" + year +
+                ", groupMember=" + groupMember +
+                '}';
+    }
+}
--- src/main/java/day12/task4/Task4.java
+++ src/main/java/day12/task4/Task4.java
@@ -1,7 +1,36 @@
 package day12.task4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task4 {
     public static void main(String[] args) {
+
+        List <String> listMember1=new ArrayList<>();
+        listMember1.add("DenisKrasko");
+        listMember1.add("DianaKrasko");
+
+        List <String> listMember2=new ArrayList<>();
+        listMember2.add("EvaKrasko");
+        listMember2.add("MarkKrasko");
+
+        List <String> listMember3=new ArrayList<>();
+        listMember3.add("ElenaKrasko");
+        listMember3.add("ValKrasko");
+
+        MusicBand mb1=new MusicBand("Skillflex",1993,listMember1);
+        MusicBand mb2=new MusicBand("Evaflex",1999,listMember2);
+        MusicBand mb3=new MusicBand("ValVortex",1981,listMember3);
+
+        List <MusicBand> listMusicBand1=new ArrayList<>();
+        listMusicBand1.add(mb1);
+        listMusicBand1.add(mb2);
+
+        List <MusicBand> listMusicBand2=new ArrayList<>();
+        listMusicBand2.add(mb3);
+
+        MusicBand.transferMembers(mb1,mb2);
 
     }
 }
