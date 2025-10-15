9f2d561a4c5d8a8551048ba79ca25743b820de32
DenisK <35003466+donz99@users.noreply.github.com>
2025-10-15 23:53:16 +0300

    Completing the Task1 day14 and day15

---
+++ src/main/java/day15/Denis2exception.java
@@ -0,0 +1,9 @@
+package day15;
+
+import day14.DenisException;
+
+public class Denis2exception extends RuntimeException {
+    public Denis2exception(String description){
+        super(description);
+    }
+}
--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,42 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File fileRead = new File("src\\main\\resources\\shoes.csv");
+        Scanner scanner = null;
 
+
+        PrintWriter pw = null;
+
+        try {
+            scanner = new Scanner(fileRead);
+            pw = new PrintWriter("src\\main\\resources\\missing_shoes.txt", StandardCharsets.UTF_8);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (fileRead.exists()&&fileRead.length()==0)
+            throw new Denis2exception("Файл пустой");
+
+
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] s = line.split(";\\s*");
+            if (s.length!=3)
+                throw new Denis2exception("Количество столбцов отличается от трёх");
+            if (Integer.parseInt(s[2]) == 0) {
+                pw.println(line);
+            }
+        }
+        pw.close();
     }
 }
